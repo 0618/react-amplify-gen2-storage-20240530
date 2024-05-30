56a1e3bc931d94234859ff069346af198663bd1e
MJ☔ <o.6180339+github@gmail.com>
2024-05-30 12:47:48 -0700

    single storage works

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { uploadData } from 'aws-amplify/storage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = React.useState({name: "test"});
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input type="file" onChange={handleChange} />
+        <button
+          onClick={() =>
+            uploadData({
+              path: `picture-submissions/${file.name}`,
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              data: file as any,
+          })
+        }
+      >
+        Upload
+      </button>
+    </div>
+  );
 }
 
 export default App
